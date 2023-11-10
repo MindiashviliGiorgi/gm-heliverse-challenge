cb5c3e1f8c46de4346bf4a39b2ed5224857753f1
Giorgi Mindiashvili <126167738+MindiashviliGiorgi@users.noreply.github.com>
2023-11-10 19:35:31 +0400

    ProjectDone

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCxSSFX2-tBwwFUW31UBqRpsR1yhmVUdfQ",
+  authDomain: "gm-heliverse-task.firebaseapp.com",
+  projectId: "gm-heliverse-task",
+  storageBucket: "gm-heliverse-task.appspot.com",
+  messagingSenderId: "184909168951",
+  appId: "1:184909168951:web:2552e4b9aa0b00bd385ab2"
+};
